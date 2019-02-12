8de90297580b02eaf39c65d442f63e215f5bfd9c
tanu_popo <d.kaneko.d@gmail.com>
2019-02-12 13:02:26 +0900

    前提条件のあるspecで仮実装

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Stack do
     subject { stack.push 1 }
     it { expect(subject).to eq stack}
   end
+  context '1をpush済のstack'do
+    before { stack.push 1}
+    describe 'lengthが1'do
+      subject { stack.length }
+      it { expect(subject).to eq 1}
+    end
+  end
 end
