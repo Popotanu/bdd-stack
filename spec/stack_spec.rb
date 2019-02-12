809970eed4305de76dafd2a5ea94357da878aa4f
tanu_popo <d.kaneko.d@gmail.com>
2019-02-12 13:09:41 +0900

    7_Specをリファクタ

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -2,25 +2,29 @@ require 'spec_helper'
 
 RSpec.describe Stack do
   let(:stack) { Stack.new }
+
   describe '#push'do
     subject { stack.push 1 }
     it { expect(subject).to eq stack}
   end
-  context '1をpush済のstack'do
-    before { stack.push 1}
-    describe 'lengthが1'do
-      subject { stack.length }
-      it { expect(subject).to eq 1}
+
+  describe '#length' do
+    context '1をpush済のstack'do
+      before { stack.push 1}
+      describe 'lengthが1'do
+        subject { stack.length }
+        it { expect(subject).to eq 1}
+      end
     end
-  end
-  context '1と5をpush済のstack' do
-    before do
-      stack.push 1
-      stack.push 5
-    end
-     describe 'lengthが2' do
-      subject { stack.length }
-      it { expect(subject).to eq 2}
+    context '1と5をpush済のstack' do
+      before do
+        stack.push 1
+        stack.push 5
+      end
+      describe 'lengthが2' do
+        subject { stack.length }
+        it { expect(subject).to eq 2}
+      end
     end
   end
 end
